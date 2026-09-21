4c84bcc6b5bbd9acbc7f69a226e59d3d75ad24b4
MinhBuiAnh <minhxuanphong@gmail.com>
2026-09-21 13:53:21 +0000

    refractor code for server initialization

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
-	handler := http.NewServeMux()
+	mux := http.NewServeMux()
+
+	filepathRoot := "."
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	port := "8080"
 	server := &http.Server{
-		Handler: handler,
-		Addr: ":8080",
+		Handler: mux,
+		Addr: ":" + port,
 	}
 	
-	server.ListenAndServe()
+	log.Printf("Starting server on port %s, serving files from %s", port, filepathRoot)
+	log.Fatal(server.ListenAndServe())
 }
